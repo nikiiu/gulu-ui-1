261f4d9e7a8b20608908ac15385d66f8d5dba673
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-10-01 16:03:48 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Uu />
+</template>
+
+<script>
+import Uu from "./components/Uu";
+
+export default {
+  name: {
+    name: "App",
+  },
+  components: { Uu },
+};
+</script>
