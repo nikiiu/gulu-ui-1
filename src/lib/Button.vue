c919e8e7181006c9479cd063cb9391fd75fe728f
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-10-04 10:47:40 +0800

    创建Button组件

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,22 @@
 <template>
-  <button>
-    <slot />
-  </button>
+  <div :size="size">
+    <button v-bind="rest">
+      <slot />
+    </button>
+  </div>
 </template>
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+  setup(props, context) {
+    const { size, level, ...rest } = context.attrs;
+
+    return { size, level, rest };
+  },
+};
+</script>
+<style lang="scss" scoped>
+div {
+  border: 1px solid red;
+}
+</style>
