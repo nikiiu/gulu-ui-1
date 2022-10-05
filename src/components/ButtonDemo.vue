7cf4a4a5f535c4d8b488b68563efcc6931bb2fc8
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-10-05 21:53:39 +0800

    button支持level

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -9,9 +9,39 @@
   </div>
   <h1>示例二</h1>
   <div>
-    <Button size="big">大大大</Button>
-    <Button>你好</Button>
-    <Button size="small">小小小</Button>
+    <div>
+      <Button size="big">大大大</Button>
+      <Button>中中中</Button>
+      <Button size="small">小小小</Button>
+    </div>
+    <div>
+      <Button theme="link" size="big">大大大</Button>
+      <Button theme="link">中中中</Button>
+      <Button size="small" theme="link">小小小</Button>
+    </div>
+    <div>
+      <Button size="big" theme="text">大大大</Button>
+      <Button theme="text">中中中</Button>
+      <Button size="small" theme="text">小小小</Button>
+    </div>
+    <h1>示例三</h1>
+    <div>
+      <div>
+        <Button level="main">主要按钮</Button>
+        <Button>普通按钮</Button>
+        <Button level="danger">危险按钮</Button>
+      </div>
+      <div>
+        <Button theme="link" level="main">主要链接按钮</Button>
+        <Button theme="link">普通链接按钮</Button>
+        <Button theme="link" level="danger">危险链接按钮</Button>
+      </div>
+      <div>
+        <Button theme="text" level="main">主要文字按钮</Button>
+        <Button theme="text">普通文字按钮</Button>
+        <Button theme="text" level="danger">危险文字按钮</Button>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
