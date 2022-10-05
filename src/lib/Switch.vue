7f379aea4a11450ee5ce4ed8f8f67060fae125aa
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-10-05 18:53:28 +0800

    css最小影响原则

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,11 @@
 <template>
-  <button @click="toggle" :class="{ checked: value }"><span></span></button>
+  <button
+    class="gulu-switch"
+    @click="toggle"
+    :class="{ 'gulu-checked': value }"
+  >
+    <span></span>
+  </button>
 </template>
 <script lang="ts">
 export default {
@@ -14,10 +20,10 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 22px;
 $h2: $h - 4px;
-button {
+.gulu-switch {
   height: $h;
   width: $h * 2;
   border: none;
@@ -25,38 +31,36 @@ button {
   border-radius: $h/2;
   position: relative;
   > span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2 / 2;
-  transition: all 250ms;
-}
-&.checked {
-  background: #1890ff;
-  > span {
-  left: calc(100% - #{$h2} - 2px);
-}
-}
- 
-&:focus {
-  outline: none;
-}
-&:active {
-  > span {
-    width: $h2 + 4px;
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: all 250ms;
+  }
+  &.gulu-checked {
+    background: #1890ff;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
+  }
+
+  &:focus {
+    outline: none;
+  }
+  &:active {
+    > span {
+      width: $h2 + 4px;
+    }
+  }
+
+  &.gulu-checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
   }
 }
-
-&.checked:active {
-  > span {
-    width: $h2 + 4px;
-    margin-left: -4px;
-  }
-}
-}
-
-
 </style>
