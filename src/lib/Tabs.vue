c7f0df8a931160a71e3441cb92761260d26d7220
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-10-08 22:15:23 +0800

    使用 css 切换内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,11 +1,36 @@
 <template>
-  <div v-for="(t, index) in titles" :key="index">{{ title }}</div>
-  <component v-for="(c, index) in defaults" :is="c" :key="index" />
+  <div class="gulu-tabs">
+    <div class="gulu-tabs-nav">
+      <div
+        class="gulu-tabs-nav-item"
+        :class="{ selected: t === selected }"
+        v-for="(t, index) in titles"
+        :key="index"
+        @click="select(t)"
+      >
+        {{ t }}
+      </div>
+    </div>
+    <div class="gulu-tabs-content">
+      <component
+        class="gulu-tabs-content-item"
+        :class="{ selected: c.props.title === selected }"
+        v-for="(c, index) in defaults"
+        :key="index"
+        :is="c"
+      />
+    </div>
+  </div>
 </template>
 <script lang="ts">
 import Tab from "./Tab.vue";
-
+import { computed } from "vue";
 export default {
+  props: {
+    selected: {
+      type: String,
+    },
+  },
   setup(props, context) {
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
@@ -13,10 +38,49 @@ export default {
         throw new Error("Tabs子标签必须是Tab");
       }
     });
+
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
-    return { defaults, titles };
+    const select = (title: string) => {
+      context.emit("update:selected", title);
+    };
+    return { defaults, titles, select };
   },
 };
 </script>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+    &-item {
+      display: none;
+      &.selected {
+        display: block;
+      }
+    }
+  }
+}
+</style>
