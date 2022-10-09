69954fafcbe4fb6a65782b81b7750b2812dc69c7
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-10-09 11:07:13 +0800

    indicator css 1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -10,6 +10,7 @@
       >
         {{ t }}
       </div>
+      <div class="gulu-tabs-nav-indicator"></div>
     </div>
     <div class="gulu-tabs-content">
       <component
@@ -24,7 +25,7 @@
 </template>
 <script lang="ts">
 import Tab from "./Tab.vue";
-import { computed } from "vue";
+
 export default {
   props: {
     selected: {
@@ -59,7 +60,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
-
+    position: relative;
     &-item {
       padding: 8px 0;
       margin: 0 16px;
@@ -71,6 +72,14 @@ $border-color: #d9d9d9;
       &.selected {
         color: $blue;
       }
+    }
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
     }
   }
   &-content {
