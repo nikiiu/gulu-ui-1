4898f0f10ff0613bc4233508daccfc0a8fb66460
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-10-10 22:58:35 +0800

    解决build后md不加载的bug

--- src/router.ts
+++ src/router.ts
@@ -8,9 +8,11 @@ import TabsDemo from "./components/TabsDemo.vue";
 import { createWebHashHistory, createRouter } from "vue-router";
 import { h } from "vue";
 import Markdown from "./components/Markdown.vue";
+import intro from "./markdown/intro.md";
+import getStarted from "./markdown/get-started.md";
+import install from "./markdown/install.md";
 const history = createWebHashHistory();
-const md = (filename) =>
-  h(Markdown, { path: `../markdown/${filename}.md`, key: filename });
+const md = (string) => h(Markdown, { content: string, key: string });
 export const router = createRouter({
   history: history,
   routes: [
@@ -22,15 +24,15 @@ export const router = createRouter({
         { path: "", redirect: "/doc/intro" },
         {
           path: "intro",
-          component: md("intro"),
+          component: md(intro),
         },
         {
           path: "get-started",
-          component: md("get-started"),
+          component: md(getStarted),
         },
         {
           path: "install",
-          component: md("install"),
+          component: md(install),
         },
         { path: "switch", component: SwitchDemo },
         { path: "button", component: ButtonDemo },
