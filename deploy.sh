c1e4f09bf1a087de07fb612fd999b8f1021a2a87
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-10-10 23:38:54 +0800

    添加一键部署脚本

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "update" &&
+git branch -M master &&
+git remote add origin git@github.com:nikiiu/uu-ui-website.git &&
+git push -f -u origin master &&
+cd -
+echo https://nikiiu.github.io/uu-ui-website/index.html
