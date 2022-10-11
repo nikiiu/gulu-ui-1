9cb1fb64e1c4c645093f05b409f532938fc6de4b
uu <11346693+nikyu77@user.noreply.gitee.com>
2022-10-11 20:59:21 +0800

    更新路径

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,15 @@
   <router-view />
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { provide, ref } from "vue";
 import { router } from "./router";
-export default {
-  name: "App",
-  setup() {
-    const width = document.documentElement.clientWidth;
-    const asideVisible = ref(width <= 500 ? false : true);
-    provide("asideVisible", asideVisible);
-    router.afterEach(() => {
-      if (width <= 500) {
-        asideVisible.value = false;
-      }
-    });
-  },
-};
+const width = document.documentElement.clientWidth;
+const asideVisible = ref(width <= 500 ? false : true);
+provide("asideVisible", asideVisible);
+router.afterEach(() => {
+  if (width <= 500) {
+    asideVisible.value = false;
+  }
+});
 </script>
